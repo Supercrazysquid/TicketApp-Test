2ffd819042a3fc154e2aa80bb6a8a9eba1fc44ce
Supercrazysquid <supercrazysquid@gmail.com>
2022-07-30 19:05:53 -0400

    Modified git ignore & added mongodb with config.json

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
+// Requires:
 const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+
+const mongoClient = mongodb.MongoClient;
+const app = express();
+
+const configPath = path.join(__dirname, 'config.json');
 
 // Create config file
-if (!fs.existsSync('./config.json')) {
-    fs.writeFileSync('./config.json', JSON.stringify({
+if (!fs.existsSync(configPath)) {
+    console.log('Creating config file...');
+
+    fs.writeFileSync(configPath, JSON.stringify({
         port: 3000,
-        db: 'mongodb://localhost/test'
+        db: {
+            host: 'localhost',
+            port: 27017,
+            database: 'test',
+            username: '',
+            password: ''
+        }
     }));
-});
+};
+
+// Load config file
+const config = require(configPath);
